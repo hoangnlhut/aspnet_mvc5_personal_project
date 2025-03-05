346a627c63bfa4ad95551d951812a3e5cb086226
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-03-05 18:35:57 +0700

    Section 1: getting started with brand new project

---
+++ 1WelcomeApp/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using _1WelcomeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1WelcomeApp.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                //Id = 1,
+                Name = "Test",
+            };
+            return View(movie);
+        }
+    }
+}
---
+++ 1WelcomeApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1WelcomeApp.Startup))]
+namespace _1WelcomeApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
