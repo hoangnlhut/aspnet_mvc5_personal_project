7f986f9967b4751468ff51930ed4ecbe042cf56a
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-03-20 12:31:42 +0700

    Section 6: Rest API: add new DTO for Customer for mapping with automapping map to entity customer

---
+++ 1WelcomeApp/Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using _1WelcomeApp.Models.CustomValidationModel;
+using _1WelcomeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _1WelcomeApp.Dtos
+{
+    public class CustomerDto
+    {
+        [Required(ErrorMessage = "Please enter customer's name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubsribedToNewsletter { get; set; }
+
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
