e6c8adb5bf0300007db2744ee33c84bb98524c82
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-03-26 11:02:04 +0700

    section 8: authentication and authorization: Restricting access

--- 1WelcomeApp/App_Start/FilterConfig.cs
+++ 1WelcomeApp/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace _1WelcomeApp
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
