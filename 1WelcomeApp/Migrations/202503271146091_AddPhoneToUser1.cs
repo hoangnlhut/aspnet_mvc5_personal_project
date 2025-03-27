b51482390ba538a94f457b76c15764d688b9702c
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-03-27 19:30:33 +0700

    section 8: authentication and authorization: Exercise Final: Add Phone to User table

---
+++ 1WelcomeApp/Migrations/202503271146091_AddPhoneToUser1.cs
@@ -0,0 +1,20 @@
+﻿namespace _1WelcomeApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddPhoneToUser1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.AspNetUsers", "DrivingLicense", c => c.String());
+            AlterColumn("dbo.AspNetUsers", "Phone", c => c.String(maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.AspNetUsers", "Phone", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.AspNetUsers", "DrivingLicense", c => c.String(nullable: false));
+        }
+    }
+}
